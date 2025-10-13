91e86f1796f5503bb357ea0bcfd6a20272c8cf81
Saaaaakibhai <hk30532@gmail.com>
2025-10-13 11:59:08 +0600

    update

--- src/main/java/bd/retailshop/retailshop_software/RetailshopSoftwareApplication.java
+++ src/main/java/bd/retailshop/retailshop_software/RetailshopSoftwareApplication.java
@@ -9,5 +9,12 @@ public class RetailshopSoftwareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RetailshopSoftwareApplication.class, args);
         System.out.println("Hello Anis Bhai");
+        System.out.println("Hello Anis Bhai");
+        System.out.println("Hello Anis Bhai");
+        System.out.println("Hello Anis Bhai");
+        System.out.println("Hello Anis Bhai");
+        System.out.println("Hello Anis Bhai");
+        System.out.println("Hello Anis Bhai");
+
 	}
 }
