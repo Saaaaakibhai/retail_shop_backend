cf648174bfc6719d3fe32b00d1d7cc1c1d0a2e27
Saaaaakibhai <hk30532@gmail.com>
2025-10-13 11:43:41 +0600

    Initial commit

---
+++ src/main/java/bd/retailshop/retailshop_software/RetailshopSoftwareApplication.java
@@ -0,0 +1,13 @@
+package bd.retailshop.retailshop_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailshopSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailshopSoftwareApplication.class, args);
+        System.out.println("Hello Anis Bhai");
+	}
+}
---
+++ src/test/java/bd/retailshop/retailshop_software/RetailshopSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.retailshop.retailshop_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailshopSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
